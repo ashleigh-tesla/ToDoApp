3ee6330acfb80b85606344479a15e8ba7f5c320e
ashleigh-tesla <84648968+ashleigh-tesla@users.noreply.github.com>
2021-09-11 06:46:00 +0200

    Added more javascript styling and functionality

--- static/js/toDoApp.js
+++ static/js/toDoApp.js
@@ -2,40 +2,20 @@ let addToDoButton = document.querySelector('#addToDo')
 let toDoContainer = document.querySelector('#toDoContainer')
 let firstInputField = document.querySelector('#firstInputField')
 let secondInputField = document.querySelector('#secondInputField')
-let newTask = document.querySelector("#firstInputField").value;
-let confirmedTask = document.querySelector("#secondInputField").value;
-let error_message = document.querySelector("#error_message");
 
-error_message.style.padding = "20px";
-
-var text;
 addToDoButton.addEventListener('click', function() {
-    if (newTask == '') {
-        text = "Please Enter New Task";
-        error_message.innerText = text;
-        error_message.style.display = "block"
-        return false
-    }
-    if (confirmedTask == '') {
-        text = "Please Confirm Task";
-        error_message.innerText = text;
-        error_message.style.display = "block"
-        return false
-    } else {
-        var paragraph = document.createElement('p')
-        paragraph.classList.add('paragraph-styling')
-        paragraph.innerText = secondInputField.value
-        toDoContainer.appendChild(paragraph)
-        firstInputField.value = ""
-        secondInputField.value = ''
-    }
+    var paragraph = document.createElement('p')
+    paragraph.classList.add('paragraph-styling')
+    paragraph.innerText = secondInputField.value
+    toDoContainer.appendChild(paragraph)
+    firstInputField.value = ""
+    secondInputField.value = ''
     paragraph.addEventListener('click', function() {
         paragraph.style.textDecoration = "line-through"
     })
     paragraph.addEventListener('dblclick', function() {
         toDoContainer.removeChild(paragraph)
     })
-    return true
 })
 
 
@@ -43,7 +23,14 @@ function validate() {
     let firstName = document.querySelector("#firstName").value;
     let secondName = document.querySelector("#secondName").value;
     let exampleDataList = document.querySelector("#exampleDataList").value;
+    let newTask = document.querySelector("#firstInputField").value;
+    let confirmedTask = document.querySelector("#secondInputField").value;
+    let error_message = document.querySelector("#error_message");
 
+    error_message.style.padding = "20px";
+    error_message.style.display = "block";
+
+    var text;
     if (firstName.length < 1) {
         text = "Please Enter Valid First Name";
         error_message.innerText = text;
@@ -59,6 +46,17 @@ function validate() {
         error_message.innerText = text;
         return false;
     }
+    if (newTask == '') {
+        text = "Please Enter New Task";
+        error_message.innerText = text;
+        return false;
+    }
+    if (confirmedTask == '') {
+        text = "Please Confirm Task";
+        error_message.innerText = text;
+        return false;
+    }
+    document.writeln(Date())
     alert("Form Submitted Successfully!");
     return true;
 }
