9ec9d115ea4f0aee4faf106d5746447eb237e88c
ashleigh-tesla <84648968+ashleigh-tesla@users.noreply.github.com>
2021-09-29 15:43:30 +0200

    Extended the User class to Operator class

--- static/js/toDoApp.js
+++ static/js/toDoApp.js
@@ -1,16 +1,12 @@
 class User {
-    constructor(firstName, secondName, exampleDataList, newTask, confirmedTask) {
+    constructor(firstName, secondName, exampleDataList) {
         this._firstName = firstName;
         this._secondName = secondName;
         this._exampleDataList = exampleDataList;
-        this._newTask = newTask;
-        this._confirmedTask = confirmedTask;
 
         firstName = null
         secondName = null
         exampleDataList = null
-        newTask = null
-        confirmedTask = null
     }
 
     set firstName(value) {
@@ -52,6 +48,113 @@ class User {
     }
     get exampleDataList() {
         return this._exampleDataList;
+    }
+}
+
+class Operator extends User {
+    constructor(firstName, secondName, exampleDataList, newTask, confirmedTask) {
+        super(firstName, secondName, exampleDataList)
+        this._newTask = newTask;
+        this._confirmedTask = confirmedTask;
+
+        newTask = ""
+        confirmedTask = ''
+
+        let saveToDoButton = document.querySelector('#saveToDo')
+        let viewToDoButton = document.querySelector('#viewToDo')
+        let sortToDoButton = document.querySelector('#sortToDo')
+        let toDoContainer = document.querySelector('#toDoContainer')
+        let toDoOrderedList = document.querySelector('#toDoOrderedList')
+        let firstInputField = document.querySelector('#firstInputField')
+        let secondInputField = document.querySelector('#secondInputField')
+
+
+        saveToDoButton.addEventListener('click', function() {
+            // get data from input box
+            var first = firstInputField.value
+            var new_data = secondInputField.value
+
+            // if there is nothing saved at the start then save an empty array
+            if (localStorage.getItem('data') == null) {
+                localStorage.setItem('data', '[]')
+            } else if (first == '' && new_data == '') {
+                localStorage.setItem('data', '[]')
+                let text = "Please Fill In Thy Today's Task"
+                console.log(text)
+                alert(text);
+            } else if (first == '' && new_data != '') {
+                localStorage.setItem('data', '[]')
+                let text = "Please Input New Task"
+                console.log(text)
+                alert(text);
+            } else if (first != '' && new_data == '') {
+                localStorage.setItem('data', '[]')
+                let text = "Please Confirm New Task"
+                console.log(text)
+                alert(text);
+            } else if (first != new_data) {
+                localStorage.setItem('data', '[]')
+                let text = "Tasks Did Not Match"
+                console.log(text)
+                alert("Tasks Did Not Match");
+            } else if (first == new_data && first != '' && new_data != '') {
+                alert('Task Added')
+
+                // get old data and slap it to the new data
+                var old_data = JSON.parse(localStorage.getItem('data'))
+                old_data.push(new_data)
+
+                // clear inputs
+                firstInputField.value = ""
+                secondInputField.value = null
+
+                // save the old + new data to local storage
+                localStorage.setItem('data', JSON.stringify(old_data))
+                let myData = JSON.parse(localStorage.getItem('data'))
+                console.log(myData)
+            }
+        })
+
+        viewToDoButton.addEventListener('click', function myFunction() {
+            toDoContainer.style.display = 'flex'
+
+            // if there is indeed data then continue
+            if (localStorage.getItem('data') != null) {
+                let paragraph = document.createElement('p')
+                paragraph.classList.add('paragraph-styling')
+                paragraph.innerText = JSON.parse(localStorage.getItem('data')).splice(-1)[0]
+                toDoContainer.appendChild(paragraph)
+
+                paragraph.addEventListener('click', function() {
+                    paragraph.style.textDecoration = "line-through"
+                    localStorage.remove();
+                })
+                paragraph.addEventListener('dblclick', function() {
+                    toDoContainer.removeChild(paragraph)
+                    localStorage.clear()
+                })
+            }
+        })
+
+        sortToDoButton.addEventListener('click', function() {
+            toDoContainer.style.display = 'flex'
+            toDoOrderedList.style.display = 'block'
+            let myData = JSON.parse(localStorage.getItem('data'))
+            sortedData = myData.sort(function(a, b) {
+                return a.localeCompare(b); //using String.prototype.localCompare()
+            });
+
+            const myList = new Vue({
+                el: '#toDoSortedContainer',
+                data: {
+                    items: sortedData
+                }
+            })
+
+            console.log(myData)
+            console.log(sortedData)
+
+        })
     }
 
     set newTask(value) {
@@ -107,7 +210,7 @@ function validateUser(event) {
     u.style.padding = "20px";
     u.style.display = "block";
 
-    const newUser = new User(v, w, x, y, z);
+    const newUser = new Operator(v, w, x, y, z);
 
     newUser.firstName = v
     newUser.secondName = w
@@ -119,99 +222,3 @@ function validateUser(event) {
     document.writeln(new Date())
     alert("Form Submitted Successfully!");
 }
-
-let saveToDoButton = document.querySelector('#saveToDo')
-let viewToDoButton = document.querySelector('#viewToDo')
-let sortToDoButton = document.querySelector('#sortToDo')
-let toDoContainer = document.querySelector('#toDoContainer')
-let toDoOrderedList = document.querySelector('#toDoOrderedList')
-let firstInputField = document.querySelector('#firstInputField')
-let secondInputField = document.querySelector('#secondInputField')
-
-
-saveToDoButton.addEventListener('click', function() {
-    // get data from input box
-    var first = firstInputField.value
-    var new_data = secondInputField.value
-
-    // if there is nothing saved at the start then save an empty array
-    if (localStorage.getItem('data') == null) {
-        localStorage.setItem('data', '[]')
-    } else if (first == '' && new_data == '') {
-        localStorage.setItem('data', '[]')
-        let text = "Please Fill In Thy Today's Task"
-        console.log(text)
-        alert(text);
-    } else if (first == '' && new_data != '') {
-        localStorage.setItem('data', '[]')
-        let text = "Please Input New Task"
-        console.log(text)
-        alert(text);
-    } else if (first != '' && new_data == '') {
-        localStorage.setItem('data', '[]')
-        let text = "Please Confirm New Task"
-        console.log(text)
-        alert(text);
-    } else if (first != new_data) {
-        localStorage.setItem('data', '[]')
-        let text = "Tasks Did Not Match"
-        console.log(text)
-        alert("Tasks Did Not Match");
-    } else if (first == new_data && first != '' && new_data != '') {
-        alert('Task Added')
-
-        // get old data and slap it to the new data
-        var old_data = JSON.parse(localStorage.getItem('data'))
-        old_data.push(new_data)
-
-        // clear inputs
-        firstInputField.value = ""
-        secondInputField.value = null
-
-        // save the old + new data to local storage
-        localStorage.setItem('data', JSON.stringify(old_data))
-        let myData = JSON.parse(localStorage.getItem('data'))
-        console.log(myData)
-    }
-})
-
-viewToDoButton.addEventListener('click', function myFunction() {
-    toDoContainer.style.display = 'flex'
-
-    // if there is indeed data then continue
-    if (localStorage.getItem('data') != null) {
-        let paragraph = document.createElement('p')
-        paragraph.classList.add('paragraph-styling')
-        paragraph.innerText = JSON.parse(localStorage.getItem('data')).splice(-1)[0]
-        toDoContainer.appendChild(paragraph)
-
-        paragraph.addEventListener('click', function() {
-            paragraph.style.textDecoration = "line-through"
-            localStorage.remove();
-        })
-        paragraph.addEventListener('dblclick', function() {
-            toDoContainer.removeChild(paragraph)
-            localStorage.clear()
-        })
-    }
-})
-
-sortToDoButton.addEventListener('click', function() {
-    toDoContainer.style.display = 'flex'
-    toDoOrderedList.style.display = 'block'
-    let myData = JSON.parse(localStorage.getItem('data'))
-    sortedData = myData.sort(function(a, b) {
-        return a.localeCompare(b); //using String.prototype.localCompare()
-    });
-
-    const myList = new Vue({
-        el: '#toDoSortedContainer',
-        data: {
-            items: sortedData
-        }
-    })
-
-    console.log(myData)
-    console.log(sortedData)
-
-})
